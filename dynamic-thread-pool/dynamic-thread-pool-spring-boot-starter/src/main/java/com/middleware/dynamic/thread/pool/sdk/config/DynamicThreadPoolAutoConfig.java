ecc8f217c79465f228674decf6ee72e961460c6a
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2024-11-05 01:37:18 -0500

    Update 11.5
    Update 11.5

--- dynamic-thread-pool/dynamic-thread-pool-spring-boot-starter/src/main/java/com/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool/dynamic-thread-pool-spring-boot-starter/src/main/java/com/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -1,7 +1,15 @@
 package com.middleware.dynamic.thread.pool.sdk.config;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @description dynamic config entrance
@@ -11,9 +19,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamicThreadPoolAutoConfig {
-    //private final
+    private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolAutoConfig.class);
     @Bean("dynamicThreadPoolService")
-    public String dynamicThreadPoolService() {
+    public String dynamicThreadPoolService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
+        String applicationName = applicationContext.getEnvironment().getProperty("spring.application.name");
+
+        if (StringUtils.isBlank(applicationName)) {
+            applicationName = "NameIsNotFound";
+            logger.warn("Dynamic thread pool startup prompt: SpringBoot application is not configured with spring.application.name, unable to obtain the application name!");
+        }
+
+        logger.info("Thread info: {}", JSON.toJSONString(threadPoolExecutorMap.keySet()));
+
         return new String("");
     }
 }
