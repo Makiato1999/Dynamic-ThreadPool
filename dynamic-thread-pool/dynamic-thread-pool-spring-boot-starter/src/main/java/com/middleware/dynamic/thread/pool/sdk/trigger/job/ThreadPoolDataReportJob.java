8ee11fdf40940ceaec4ad340035298beb80fd91b
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2024-11-17 21:49:46 -0500

    Update ThreadPoolDataReportJob.java
    Update 11.17

--- dynamic-thread-pool/dynamic-thread-pool-spring-boot-starter/src/main/java/com/middleware/dynamic/thread/pool/sdk/trigger/job/ThreadPoolDataReportJob.java
+++ dynamic-thread-pool/dynamic-thread-pool-spring-boot-starter/src/main/java/com/middleware/dynamic/thread/pool/sdk/trigger/job/ThreadPoolDataReportJob.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import java.util.List;
 
 /**
- * @description 线程池数据上报任务
+ * @description 线程池数据定时上报任务，用于报告线程池及其配置参数信息
  * @author Xiaoran
  * @create 2024-11-17
  */
@@ -30,11 +30,15 @@ public class ThreadPoolDataReportJob {
 
     @Scheduled(cron = "0/20 * * * * ?")
     public void execReportThreadPoolList() {
+        // 获取所有动态线程池的配置信息列表, 列表包含了多个 ThreadPoolConfigEntity 对象，每个对象对应一个线程池的配置
         List<ThreadPoolConfigEntity> threadPoolConfigEntityList = dynamicThreadPoolService.queryThreadPoolList();
+        // 将获取到的列表上报到注册中心
         registry.reportThreadPool(threadPoolConfigEntityList);
         logger.info("Dynamic thread pool, report thread pool information: {}", JSON.toJSONString(threadPoolConfigEntityList));
 
+        // 遍历每个线程池的配置信息
         for (ThreadPoolConfigEntity threadPoolConfigEntity : threadPoolConfigEntityList) {
+            // 上报每个线程池的详细配置参数
             registry.reportThreadPoolConfigParameter(threadPoolConfigEntity);
             logger.info("Dynamic thread pool, report thread pool configuration: {}", JSON.toJSONString(threadPoolConfigEntity));
         }
